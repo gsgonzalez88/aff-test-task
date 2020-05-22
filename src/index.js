c2690159462c729ba748984aa445df810c84ba97
Gerardo Gonzalez <gerardo.gonzalez@naranja.com>
2020-05-22 20:39:57 -0300

    added db connection to server

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
+const myConnection = require('express-myconnection');
+const mysql = require('mysql');
+const path = require('path');
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
 
+// parse application/json
+app.use(bodyParser.json());
+
+const publicDir = path.join(__dirname,'../public');
+app.use(express.static(publicDir));
+
+app.use(myConnection(mysql,{
+    host: 'sql10.freemysqlhosting.net',
+    database: 'sql10342273',
+    user: '',
+    password: ''    
+}, 'single'));
+
+
+//setting environment port
 const port = process.env.PORT || 8080;
 
-
-app.listen(port);
 console.log(`Server Listening on port: ${port}`);
+app.listen(port);
